589ba1ad289fc3091a6fe32792ce5adb3e70a6b5
DonovanDwyer <dwyer.donovan@gmail.com>
2018-10-11 18:00:37 -0400

    defeated the final boss

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,38 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    @figures = Figure.all
+    erb :'/landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    @figures = Figure.all
+    erb :'landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    #binding.pry
+    @landmark = Landmark.find(params[:id])
+    @landmark.name = params[:landmark][:name]
+    @landmark.year_completed = params[:landmark][:year_completed]
+    # @figure = Figure.create(params["figure"]["name"])
+    # @landmark.figure_id = @figure.id
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
+  end
 end
